8d97ab2277f92707c03d6831f779029eb18f6572
JavaDhruv <redhudhruv185@gmail.com>
2026-01-10 20:51:44 +0530

    .

--- teamflow/src/main/java/com/dhruv/teamflow/config/SecurityConfig.java
+++ teamflow/src/main/java/com/dhruv/teamflow/config/SecurityConfig.java
@@ -9,15 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form.disable());
-
-        return http.build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
