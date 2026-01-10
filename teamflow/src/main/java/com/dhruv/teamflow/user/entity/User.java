fc674a75133436155f2e3b7f93cf788969960ffa
JavaDhruv <redhudhruv185@gmail.com>
2026-01-10 20:44:10 +0530

    UniqueConstraints

--- teamflow/src/main/java/com/dhruv/teamflow/user/entity/User.java
+++ teamflow/src/main/java/com/dhruv/teamflow/user/entity/User.java
@@ -7,6 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
 @Getter
 @Setter
 @NoArgsConstructor
