f0ac36cddb829e4ddc42e556d3611bcf8a3bb214
JavaDhruv <redhudhruv185@gmail.com>
2026-01-10 20:05:51 +0530

    OrganizationService

--- teamflow/src/main/java/com/dhruv/teamflow/organization/controller/OrganizationController.java
+++ teamflow/src/main/java/com/dhruv/teamflow/organization/controller/OrganizationController.java
@@ -31,4 +31,21 @@ public class OrganizationController {
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+    @GetMapping
+    public ResponseEntity<List<OrganizationResponse>> getAllOrganizations() {
+        List<OrganizationResponse> response = organizationService
+                .getAllOrganizations()
+                .stream()
+                .map(org -> new OrganizationResponse(org.getId(), org.getName()))
+                .toList();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrganizationResponse> getOrganizationById(@PathVariable Long id) {
+        var org = organizationService.getOrganizationById(id);
+        return ResponseEntity.ok(new OrganizationResponse(org.getId(), org.getName()));
+    }
+
 }
